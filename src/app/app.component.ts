ff044c14740bad4367663a5b1e4fbd821ea7dbb1
Steven Murray <32107837+SMinTexas@users.noreply.github.com>
2018-10-04 21:16:58 -0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
+import { CounterService } from './services/counter.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  activeUsers = [];
+  inactiveUsers = [];
+
+  constructor(private userService: UsersService,
+              private counterService: CounterService) {}
+
+  ngOnInit() {
+    this.activeUsers = this.userService.getActiveUsers();
+    this.inactiveUsers = this.userService.getInactiveUsers();
+  }
+
+  getTransitionCount() {
+    return this.counterService.getTransitionCount();
+  }
+}
